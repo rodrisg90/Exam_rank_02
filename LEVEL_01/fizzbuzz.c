7e9be80ddb4b78575249640d012c49d37b18d494
rodrisg90 <102601255+rodrisg90@users.noreply.github.com>
2022-09-20 16:03:33 +0200

    Update fizzbuzz

--- LEVEL_01/fizzbuzz.c
+++ LEVEL_01/fizzbuzz.c
@@ -1,10 +1,13 @@
 #include <unistd.h>
 
-void    ft_write_nb(int nb)
+void ft_putnbr(int nb)
 {
+    char c;
+
     if (nb > 9)
-        ft_write_nb(nb / 10);
-    write (1, &"0123456789"[nb % 10], 1);
+        ft_putnbr(nb / 10);
+    c = (nb % 10) + '0';
+    write(1, &c, 1);
 }
 
 int main(void)
@@ -14,15 +17,15 @@ int main(void)
     nb = 1;
     while (nb <= 100)
     {
-        if (nb % 5 == 0 && nb % 3 == 0)
-            write (1, "fizzbuzz", 8);
+        if (nb % 3 == 0 && nb % 5 == 0)
+            write(1, "fizzbuzz", 8);
         else if (nb % 3 == 0)
-            write (1, "fizz", 4);
+            write(1, "fizz", 4);
         else if (nb % 5 == 0)
-            write (1, "buzz", 4);
+            write(1, "buzz", 4);
         else
-            ft_write_nb(nb);
+            ft_putnbr(nb);
+        write(1, "\n", 1);
         nb++;
-        write (1, "\n", 1);
     }
 }
